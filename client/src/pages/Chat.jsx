3a2ddef159e751c29374385a51024a1a129c3601
Moshe Azraf <moshiko458@gmail.com>
2024-01-07 17:28:19 +0200

    basic layout

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -6,11 +6,13 @@ import { useSelector } from 'react-redux';
 
 
 const Chat = () => {
+  //redux states
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
   const userName = useSelector((state) => state.user.userName);
   console.log(isLoggedIn, userName)
+  
+  //react states
   const [onlineUsers, setOnlineUsers] = useState([]);
-
 
 
   useEffect(() => {
@@ -36,6 +38,27 @@ const Chat = () => {
     <div>
       <h1>chat</h1>
       <button onClick={() => console.log("users:",onlineUsers)}>get users</button>
+      <hr />
+
+      <div className='flex'>
+        <button>search users</button>
+      </div>
+      <hr />
+
+      <div className='grid grid-cols-3 items-stretch'>
+        <div>
+          <p>chat list</p>
+        </div>
+
+        {/* vartical line */}
+        <div className='w-px bg-gray-300'></div>
+
+        <div>
+          <p>current chat</p>
+        </div>
+      </div>
+
+
 
     </div>
   )
